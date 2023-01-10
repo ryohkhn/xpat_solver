07562495eada54dabfe7cb30a4bb710946a198a2
RODRIGUEZ lucas <lucasrodriguez@free.fr>
2023-01-10 22:16:20 +0100

    code cleanup

--- src/Solve.ml
+++ src/Solve.ml
@@ -1,87 +1,7 @@
 open State
 open Card
 
-(*
-legal_moves state game donne les coups possibles depuis un state
-
-dans la fonction principale:
-
--> un Set des etats deja vu
-
--> un Set des etats possibles mais pas vu (au debut: le state initial)
-
--> une boucle qui:
-
-1) choisit un etat parmi le Set possible, probablement celui avec le score le plus haut
-
-2) utilise legal_moves pour avoir toutes les etats possibles
-
-3) parmi ces etats, un par un on decide si on les ajoute au etat possible
-pour decider si on ajoute un etat, il faut etre sur que cet etat n'existe pas dans le Set deja vu
--> il faut bien le comparer avec toutes les etats deja vus ( il suffit pas de comparer les historiques )
-on va utiliser:
-module States = Set.Make (struct type t = state let compare = compare_state end)
-alors il faut bien definir compare_state
-
-4) si on decide d'ajouter cet etat, il faut mettre a jour l'historique -> l'historique d'etat precedent + le coup courant
-
-5) on enleve l'etat choisit dans l'etape 1 du set possible et on le mets dans le set deja vu
-
-on arrete la boucle dans deux cas:
-l'etat de l'etape 1 est complet -> solution trouve
-ou bien il n'y a plus d'etats possibles -> pas de solution
-
-
-Possibles ameliorations:
-"si on a déjà trouvé un état de score 30, il est en effet peu probable que les états de score moins de 20 soit encore utiles pour cette recherche
-(on pourra alors paramétrer cet écart via une option du programme). Une telle "distance d'oubli". "
-*)
-
-
-(*
-Maintenant, quelle comparaison compare_state doit-on utiliser ?
-OCaml propose une comparaison générique nommée Stdlib.compare qui est souvent convenable.
-Mais ici on vous propose de mettre un historique des coups dans chaque état.
-Or deux historiques différents peuvent pourtant mener à des états
-rigoureusement identiques à part ça (mêmes colonnes, mêmes registres, et donc mêmes dépôts).
-Pour le bon fonctionnement de l'algorithme de recherche, il est alors crucial que dans ce cas compare_state réponde une égalité (code 0).
-Bref, pour compare_state, au lieu de comparer deux états entiers directement via Stdlib.compare,
-on pourra comparer leurs zones de registres respectives (p.ex. par Stdlib.compare),
-et en cas d'égalité seulement comparer leurs zones de colonnes respectives (via un autre Stdlib.compare). Et rien de plus.
-*)
-
-let get_scores state =
-  List.fold_left (+) 0 (state.depot)
-
 let compare_state a b =
-  let comp = Stdlib.compare a.registres b.registres in
-  if (comp = 0) then
-    Stdlib.compare a.colonnes b.colonnes
-  else comp
-(*
-  if (get_scores a <> get_scores b) then 1 else
-  if(Stdlib.compare a.history b.history = 0) then
-    0
-  else
-    let rec compare_cols n =
-      if n = a.nbCol then 0
-      else
-        let x = FArray.get a.colonnes n in
-        if FArray.exists (fun y -> (Stdlib.compare x y = 0)) (b.colonnes) then compare_cols (n+1)
-        else 1
-    in
-    let rec compare_regs n =
-      if a.registres = None then (if b.registres = None then (compare_cols 0) else 1)
-      else if n = a.nbReg then compare_cols 0
-      else
-        let x = FArray.get (Option.get a.registres) n in
-        if FArray.exists (fun y -> (Stdlib.compare x y = 0))
-             (Option.get b.registres) then compare_regs (n+1)
-        else 1
-    in compare_regs 0
-*)
-
-let compare_state' a b =
   if(Stdlib.compare a.history b.history = 0) then
     0
   else
@@ -94,6 +14,14 @@ let compare_state' a b =
 
 
 module States = Set.Make (struct type t = state let compare = compare_state end)
+
+
+let print_states states =
+  Printf.printf "Taille %d\n" (List.length (States.elements states))
+
+
+let get_score state =
+  List.fold_left (+) 0 (state.depot)
 
 
 let legal_moves_to_registers state =
@@ -116,6 +44,7 @@ let legal_moves_to_registers state =
             reg_moves state (n+1) (move::acc)
       in reg_moves state 0 []
   else []
+
 
 let legal_moves_to_empty state list game=
   (* In midnight oil and baker's dozen no moves to empty column *)
@@ -142,6 +71,7 @@ let legal_moves_to_empty state list game=
           empty_col_moves state (n+1) (move::acc)
     in empty_col_moves state 0 list
   else list
+
 
 let legal_column_moves state list game =
   let rec column_moves state n acc =
@@ -175,69 +105,25 @@ let legal_moves state game =
     legal_column_moves state (legal_moves_to_empty state list game) game
 
 
-let print_moves moves =
-  let _ = Printf.printf "Coups possibles :\n " in
-  let rec print_moves' moves =
-    match moves with
-      [] -> None
-    | h::t -> let _ = Printf.printf "%s\n" h in print_moves' t
-  in
-  print_moves' moves
-
-
-let print_states states =
-  Printf.printf "Taille %d\n" (List.length (States.elements states))
-  (*j
-  Printf.printf "Liste des états vus:\n";
-  let a = States.iter (fun s -> print_string (State.state_to_string s)) states in
-  a *)
-
-
-
-let print_history state =
-  (*Printf.printf "Historique:\n";*)
-  let hist = state.history in
-  if hist = None then
-    (
-      print_string "hist vide\n";
-    )
-  else
-    (
-    List.iter (fun s -> print_string (s ^ "; ")) (Option.get hist);
-    print_string "\n";
-  )
-
-
 let rec legal_moves_to_states seen_states possible_states initial_state moves =
   match moves with
   | [] -> possible_states
   | x::moves' ->
     let x',y' = Check.split_move x in
     let new_state = Check.process_move_unchecked initial_state x' y' in
-    (*
-    Printf.printf "Prochaine état avec le coup %s\n" x; *)
-
     let new_state = Check.normalise new_state in
     let new_state = Check.add_to_history new_state x in
-
-    (*print_string (State.state_to_string new_state);*)
-    (*print_history new_state;*)
-    (*
-    let res = compare_state initial_state new_state in
-    Printf.printf "compare init et new %d\n" res;
-    let _ = print_string (State.state_to_string  initial_state) in
-    let _ = print_string (State.state_to_string  new_state) in
-     *)
     if not(States.mem new_state seen_states) then
       let possible_states = States.add new_state possible_states in
       legal_moves_to_states seen_states possible_states initial_state moves'
     else
         legal_moves_to_states seen_states possible_states initial_state moves'
 
+
 let get_biggest_score possible_states =
   States.fold
     (fun x acc ->
-       if get_scores acc > get_scores x then
+       if get_score acc > get_score x then
          acc
        else
          x
@@ -245,84 +131,38 @@ let get_biggest_score possible_states =
     possible_states State.empty_state
 
 
-let rec solve_old state game seen_states possible_states =
-  (* récursion sur tous les états possibles *)
-  let rec solve'' seen_states possible_states =
-    (* si n'il y a plus d'états possibles on renvoit None *)
-    if States.is_empty possible_states then
-      None,1,seen_states
-    else
-
-      (* on choisit l'état avec le score le plus élevé *)
-      let new_state = get_biggest_score possible_states in
-      (* on retire l'état précédent de la liste des états possibles *)
-      (* let _ = Printf.printf "\n***\nAvant : " in *)
-      (* let _ = print_states possible_states in *)
-      (* let a' = States.mem new_state possible_states in *)
-      let possible_states = States.remove new_state possible_states in
-      (* let _ = Printf.printf "Apres : " in *)
-      (* let _ = print_states possible_states in *)
-
-       (* Printf.printf "Etat avec le plus grand score = %d\n" (get_scores new_state); *)
-      (* (\* on appelle récursivement solve' sur cet état *\) *)
-      (* print_string "Nouvel état :*****************************************************\n"; *)
-      (* print_string (State.state_to_string new_state); *)
-      let ret_state,value,seen_states = solve_old new_state game seen_states possible_states in
-      (* si ce chemin n'a donné aucun résultat on appelle solve'' sur l'état possible suivant *)
-      if ret_state = None then
-        solve'' seen_states possible_states
-      else
-        ret_state,value,seen_states
-  in
-  (* Si l'état est terminé alors on quitte le solveur *)
-  if get_scores state = 52 then
-    Some state,0,seen_states
-  else
-    (* on ajoute aux états vus l'état courant *)
-    let seen_states = States.add state seen_states in
-    (*print_history state;*)
-    (* on récupère tous les coups possibles *)
-    let moves = legal_moves state game in
-    (* let _ = print_moves moves in *)
-    (* on ajoute aux états possibles les états ne faisant pas partie des états vus *)
-    let possible_states =
-      legal_moves_to_states seen_states possible_states state moves
-    in
-    (* print_states seen_states;*)
-    (* Si aucune prochaine branche est possible on arrête *)
-    if States.is_empty possible_states then
-      None,1,seen_states
-    else
-      solve'' seen_states possible_states
-
 let rec solve' state game seen_states possible_states =
   let new_state = get_biggest_score possible_states in
   let moves = legal_moves new_state game in
   let possible_states = States.remove new_state possible_states in
-  let possible_states = legal_moves_to_states seen_states possible_states new_state moves in
-  let seen_states = States.add new_state seen_states 
+  let possible_states =
+    legal_moves_to_states seen_states possible_states new_state moves
   in
-  (* print_states seen_states; *)
-  if get_scores new_state = 52 then
+  let seen_states =
+    States.add new_state seen_states
+  in
+  if get_score new_state = 52 then
     Some new_state,0,seen_states
   else if States.is_empty possible_states then
     None,1,seen_states
   else
     solve' state game seen_states possible_states
 
+
 let solve_diff state game seen_states possible_states diff =
   let rec solve_diff' state game seen_states possible_states diff max =
     let new_state = get_biggest_score possible_states in
-    let tmp = get_scores new_state in
-    let max = Stdlib.max tmp max in
+    let max = Stdlib.max (get_score new_state) max in
     let moves = legal_moves new_state game in
     let possible_states = States.remove new_state possible_states in
     let possible_states = legal_moves_to_states seen_states possible_states new_state moves in
-    let possible_states = States.filter (fun s -> get_scores s > (max-diff)) possible_states in
+    let possible_states,removed_states =
+      States.partition (fun s -> get_score s > (max-diff)) possible_states
+    in
+    let seen_states = States.union removed_states seen_states in
     let seen_states = States.add new_state seen_states
     in
-    (* print_states seen_states; *)
-    if get_scores new_state = 52 then
+    if get_score new_state = 52 then
       Some new_state,0,seen_states
     else if States.is_empty possible_states then
       None,1,seen_states
@@ -332,13 +172,15 @@ let solve_diff state game seen_states possible_states diff =
   solve_diff' state game seen_states possible_states diff 0
 
 
-let solve state game =
+let solve state game diff =
   let state = Check.normalise state in
   let seen_states = States.empty in
   let possible_states = States.(empty |> add state) in
   let state,re_val,seen =
-      solve_diff state game seen_states possible_states 10
-      (* solve' state game seen_states possible_states *)
+    if (fst diff) then
+      solve_diff state game seen_states possible_states (snd diff)
+    else
+      solve' state game seen_states possible_states
   in
   print_states seen;
   (state,re_val)
--- src/XpatSolver.ml
+++ src/XpatSolver.ml
@@ -6,8 +6,8 @@ type mode =
   | Check of string (* filename of a solution file to check *)
   | Search of string (* filename where to write the solution *)
 
-type config = { mutable game : game; mutable seed: int; mutable mode: mode }
-let config = { game = Freecell; seed = 1; mode = Search "" }
+type config = { mutable game : game; mutable seed: int; mutable mode: mode; mutable diff: bool*int}
+let config = { game = Freecell; seed = 1; mode = Search ""; diff = (false,0)}
 
 let getgame = function
   | "FreeCell"|"fc" -> Freecell
@@ -35,51 +35,49 @@ let set_game_seed name =
   with _ -> failwith ("Error: <game>.<number> expected, with <game> in "^
                       "FreeCell Seahaven MidnightOil BakersDozen")
 
+let set_diff_value value =
+  try
+    let tmp = int_of_string value in
+    if tmp <= 0 then raise (Invalid_argument("Error: -diff <value> needs to be at least 1"));
+    config.diff <- (true,tmp)
+  with
+  | Invalid_argument i -> failwith(i)
+  | _ -> failwith ("Error: -diff <value> needs to be an int")
+
+
 let winning_state state =
   let depots =  State.depot state in
   depots = [13;13;13;13]
 
 let treat_game conf =
-  let _ = conf.seed <- 2 in
-  let _ = conf.game <- Baker in
-(*
-  let _ = conf.seed <- 123456 in
-  let _ = conf.game <- Baker in *)
   let permut = XpatRandom.shuffle conf.seed in
+  (*
   Printf.printf "Voici juste la permutation de graine %d:\n" conf.seed;
   List.iter (fun n -> print_int n; print_string " ") permut;
   print_newline ();
   List.iter (fun n -> Printf.printf "%s " (Card.to_string (Card.of_num n)))
-    permut;
+    permut; *)
   print_newline ();
   Printf.printf "Game %s with seed %d \n"
     (game_to_string conf.game) (conf.seed) ;
-
-
-
-  (*Temporary tests -> to remove*)
-  let s = State.create_state
-            (game_to_string conf.game) permut in
-  Printf.printf "%s" (State.state_to_string s) ;
-  (*let _ = conf.mode <- Search "out.sol" in*)
-
-
+  print_newline ();
+  let s = State.create_state (game_to_string conf.game) permut in
+  Printf.printf "État initial de la partie \n%s" (State.state_to_string s) ;
 
   match conf.mode with
   | Check x ->
-     let res,n = (Check.check x s (game_to_string conf.game)) in
-     if res = None || Option.get res = s then Printf.printf "ECHEC %d" n
-     else if winning_state (Option.get res) then Printf.printf "SUCCES"
-     else Printf.printf "ECHEC %d" n
-     ; exit n
+    let res,n = (Check.check x s (game_to_string conf.game)) in
+    if res = None || Option.get res = s then Printf.printf "ECHEC %d" n
+    else if winning_state (Option.get res) then Printf.printf "SUCCES"
+    else Printf.printf "ECHEC %d" n
+       ; exit n
   | Search x ->
-     let res,n = (Solve.solve s (game_to_string conf.game)) in
-     if n = 1 then Printf.printf "INSOLUBLE"
-     else let _ = Printf.printf "SUCCES" in
-          let oc = open_out x in
-          Printf.fprintf oc "%s\n" (State.history_to_string (Option.get res).history);
-     (*TODO handle n = 2 , write to out.sol *)
-     ; exit n
+    let res,n = (Solve.solve s (game_to_string conf.game) config.diff) in
+    if n = 1 then Printf.printf "INSOLUBLE"
+    else let _ = Printf.printf "SUCCES" in
+      let oc = open_out x in
+      Printf.fprintf oc "%s\n" (State.history_to_string (Option.get res).history);
+      ; exit n
 
 
 let main () =
@@ -87,7 +85,11 @@ let main () =
     [("-check", String (fun filename -> config.mode <- Check filename),
         "<filename>:\tValidate a solution file");
      ("-search", String (fun filename -> config.mode <- Search filename),
-        "<filename>:\tSearch a solution and write it to a solution file")]
+        "<filename>:\tSearch a solution and write it to a solution file");
+       ("-diff", String (fun filename -> set_diff_value filename),
+        "<value>:\tValue of the difference.
+                        Possible states with a difference of <value> from the maximum score achieved will be deleted.
+                        The results are less exhaustive and generally tend towards no solution.")]
     set_game_seed (* pour les arguments seuls, sans option devant *)
     "XpatSolver <game>.<number> : search solution for Xpat2 game <number>";
   treat_game config
