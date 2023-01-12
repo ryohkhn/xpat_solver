2aecfb59fdb07d02dad3f7fbad367989568a1ef3
RODRIGUEZ lucas <lucasrodriguez@free.fr>
2023-01-12 21:08:46 +0100

    Fix moves calculation

--- src/Solve.ml
+++ src/Solve.ml
@@ -6,15 +6,10 @@ Elle compare d'abord l'historique des deux états,
 les retire puis compare les registres.
 Si les registres sont identiques on compare les colonnes. *)
 let compare_state a b =
-  if(Stdlib.compare a.history b.history = 0) then
-    0
-  else
-    let a = State.cpy_state_nohist a in
-    let b = State.cpy_state_nohist b in
-    let comp = Stdlib.compare a.registres b.registres in
-    if (comp = 0) then
-      Stdlib.compare a.colonnes b.colonnes
-    else comp
+  let comp = Stdlib.compare a.registres b.registres in
+  if (comp = 0) then
+    Stdlib.compare a.colonnes b.colonnes
+  else comp
 
 
 module States = Set.Make (struct type t = state let compare = compare_state end)
@@ -30,15 +25,24 @@ let get_score state =
   List.fold_left (+) 0 (state.depot)
 
 
-(* Retourne la liste coups possibles vers les registres *)
+(* Vérifie si une des colonnes est vide *)
+let rec free_columns state n =
+  if n = state.nbCol then false
+  else if FArray.get state.colonnes n = [] then
+    true
+  else free_columns state (n+1)
+
+
+(* Vérifie si un des registres est libre *)
+let rec free_registres state n =
+  if n = state.nbReg then false
+  else if (FArray.get (Option.get state.registres) n) = None then
+    true
+  else free_registres state (n+1)
+
+
+(* Retourne la liste des coups possibles vers les registres *)
 let legal_moves_to_registers state =
-  (* Vérifie si un des registres est libre *)
-  let rec free_registres state n =
-    if n = state.nbReg then false
-    else if (FArray.get (Option.get state.registres) n) = None then
-      true
-    else free_registres state (n+1)
-  in
   if(free_registres state 0) then
     (* Iterate column heads for possible moves towards registry *)
     let rec reg_moves state n acc =
@@ -55,8 +59,19 @@ let legal_moves_to_registers state =
     reg_moves state 0 []
   else []
 
+(** Retourne la liste des coups possibles vers
+ une colonne vide avec la carte en argument *)
+let process_move_to_empty fun_rec game card n acc =
+  (* In seahaven no moves to empty column except if the card is a king *)
+  if game = "Seahaven"
+  && fst(card) <> 13 then
+    fun_rec (n+1) acc
+  else
+    let move = (string_of_int (to_num card)) ^ " V" in
+    fun_rec (n+1) (move::acc)
 
-(* Retourne la liste coups possibles vers les colonnes vides *)
+
+(* Retourne la liste des coups possibles vers les colonnes vides *)
 let legal_moves_to_empty state list game =
   (* In midnight oil and baker's dozen no moves to empty column *)
   if (game = "BakersDozen" || game = "bd") then
@@ -65,71 +80,105 @@ let legal_moves_to_empty state list game =
   if (game = "MidnightOil" || game = "mo") then
     list
   else
-    (* Vérifie si une des colonnes est vide *)
-    let rec free_columns state n =
-      if n = state.nbCol then false
-      else if FArray.get state.colonnes n = [] then
-        true
-      else free_columns state (n+1)
+  if(free_columns state 0) then
+    let rec reg_col_moves reg n acc =
+      if n = state.nbReg then acc
+      else
+        let card_op = FArray.get reg n in
+        if card_op = None then
+          reg_col_moves reg (n+1) acc
+        else
+          let card = Option.get card_op in
+          process_move_to_empty (reg_col_moves reg) game card n acc
+          (*
+             if game = "Seahaven"
+             && fst(card) <> 13 then
+             process_move_to_empty (reg_col_moves reg) game card n acc
+             reg_col_moves reg (n+1) acc
+             else
+             let move = (string_of_int (to_num card)) ^ " V" in
+             process_move_to_empty (reg_col_moves reg) game card n acc
+             reg_col_moves reg (n+1) (move::acc) *)
     in
-    if(free_columns state 0) then
-      let rec empty_col_moves state n acc =
-        if n = state.nbCol then acc
-        else
-        if List.length (FArray.get state.colonnes n) < 2
-        then empty_col_moves state (n+1) acc
-        else
-          let card = (List.hd(FArray.get state.colonnes n)) in
-          (* In seahaven no moves to empty column except if the card is a king *)
-          if ( (game = "Seahaven" || game = "st" )
-               && fst(of_num card) <> 13) then
-            empty_col_moves state (n+1) acc
-          else
-            let move = (string_of_int card) ^ " V" in
-            empty_col_moves state (n+1) (move::acc)
-      in empty_col_moves state 0 list
-    else list
-
-
-(* Retourne la liste coups possibles vers les colonnes *)
-let legal_column_moves state list game =
-  let rec column_moves state n acc =
-     if n = state.nbCol then acc
-     else if FArray.get state.colonnes n = [] then
-       column_moves state (n+1) acc
-     else
-       let card = (List.hd(FArray.get state.colonnes n)) in
-       let rec moves_aux state x card acc2 =
-         if x = state.nbCol then acc2
-         else
-         if FArray.get state.colonnes x = [] then
-           moves_aux state (x+1) card acc2
-         else
-           let dest_card = (List.hd(FArray.get state.colonnes x)) in
-           if card = dest_card then
-             moves_aux state (x+1) card acc2
+    let rec empty_col_moves state n acc =
+      if n = state.nbCol then
+        if state.registres <> None then
+          reg_col_moves (Option.get state.registres) 0 acc
+        else acc
+      else
+      if List.length (FArray.get state.colonnes n) < 2
+      then empty_col_moves state (n+1) acc
+      else
+        let card = (List.hd(FArray.get state.colonnes n)) in
+        process_move_to_empty (empty_col_moves state) game (of_num card) n acc
+        (*
+           (* In seahaven no moves to empty column except if the card is a king *)
+           if (game = "Seahaven"
+             && fst(of_num card) <> 13) then
+           empty_col_moves state (n+1) acc
            else
-             let move = (string_of_int card) ^ " " ^
-                          (string_of_int dest_card) in
-             let result =
-               Check.verify_move (string_of_int card)
-                 (string_of_int dest_card) game
-             in
-             let new_acc =
-               if result then
-                 (move::acc2)
-               else
-                 acc2
-             in moves_aux state (x+1) card new_acc
-       in column_moves state (n+1) (moves_aux state 0 card acc)
-  in column_moves state 0 list
+           let move = (string_of_int card) ^ " V" in
+           empty_col_moves state (n+1) (move::acc) *)
+    in
+    empty_col_moves state 0 list
+  else list
+
+
+(** Retourne la liste des coups possibles vers
+  les colonnes avec la carte en argument *)
+let rec process_move_to_col state x card acc2 game =
+  if x = state.nbCol then acc2
+  else
+  if FArray.get state.colonnes x = [] then
+    process_move_to_col state (x+1) card acc2 game
+  else
+    let dest_card = (List.hd(FArray.get state.colonnes x)) in
+    if card = dest_card then
+      process_move_to_col state (x+1) card acc2 game
+    else
+      let move = (string_of_int card) ^ " " ^
+                 (string_of_int dest_card)
+      in
+      let result =
+        Check.verify_move (string_of_int card)
+          (string_of_int dest_card) game
+      in
+      let new_acc =
+        if result then
+          (move::acc2)
+        else
+          acc2
+      in
+      process_move_to_col state (x+1) card new_acc game
+
+
+(* Retourne la liste des coups possibles vers les colonnes *)
+let legal_column_moves state list game =
+  let rec reg_moves state n acc =
+    if n = state.nbReg then acc
+    else
+      let card_op = FArray.get (Option.get state.registres) n in
+      if card_op = None then
+        reg_moves state (n+1) acc
+      else
+        let card = to_num (Option.get card_op) in
+        reg_moves state (n+1) (process_move_to_col state 0 card acc game)
+  in
+  let rec column_moves state n acc =
+    if n = state.nbCol then reg_moves state 0 acc
+    else if FArray.get state.colonnes n = [] then
+      column_moves state (n+1) acc
+    else
+      let card = (List.hd(FArray.get state.colonnes n)) in
+      column_moves state (n+1) (process_move_to_col state 0 card acc game)
+  in
+  column_moves state 0 list
 
 
 (* Fonction générale de calcul des coups légaux *)
 let legal_moves state game =
-  let list =
-    legal_moves_to_registers state in
-    legal_column_moves state (legal_moves_to_empty state list game) game
+  let list = legal_moves_to_registers state in
+  legal_column_moves state (legal_moves_to_empty state list game) game
 
 
 (* Transforme une liste de coups légaux et l'ajoute au Set d'états atteignables *)
@@ -180,12 +229,12 @@ let rec solve' state game seen_states possible_states =
     States.add new_state seen_states
   in
   if get_score new_state = 52 then
-    Some new_state,0
-  else if States.is_empty possible_states then
     (
     print_states_size seen_states;
-    None,2
+    Some new_state,0
   )
+  else if States.is_empty possible_states then
+    None,2
   else
     solve' state game seen_states possible_states
 
@@ -211,12 +260,12 @@ let solve_diff state game seen_states possible_states diff =
     let seen_states = States.union removed_states seen_states in
     let seen_states = States.add new_state seen_states in
     if get_score new_state = 52 then
-      Some new_state,0
-    else if States.is_empty possible_states then
       (
       print_states_size seen_states;
-      None,1
+      Some new_state,0
     )
+    else if States.is_empty possible_states then
+      None,1
     else
       solve_diff' state game seen_states possible_states diff max
   in
